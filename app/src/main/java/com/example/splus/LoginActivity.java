5d0e0f9e427abadcf0052fcfa4664c77f6431afa
Trinh Dang Duong <duongtd0109@gmail.com>
2023-04-15 03:43:53 +0700

    Add Splash Activity & Setting Account Activity (undone)

--- app/src/main/java/com/example/splus/LoginActivity.java
+++ app/src/main/java/com/example/splus/LoginActivity.java
@@ -2,14 +2,14 @@ package com.example.splus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,6 +25,8 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText editTextUsername, editTextPassword;
     Button loginButton;
+    TextView leadToSignUp;
+    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
         loginButton = findViewById(R.id.buttonLogin);
+        leadToSignUp = findViewById(R.id.textLeadToSignUp);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +44,7 @@ public class LoginActivity extends AppCompatActivity {
                 while (index < numOfPair) {
                     if (editTextUsername.getText().toString().equals(username[index])) {
                         if (editTextPassword.getText().toString().equals(password[index])) {
-                            Toast.makeText(LoginActivity.this, "R.string.loginSuccessful", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, R.string.loginSuccessful, Toast.LENGTH_SHORT).show();
                             Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(mainIntent);
                             break;
@@ -50,8 +53,16 @@ public class LoginActivity extends AppCompatActivity {
                     index++;
                 }
                 if (index == numOfPair) {
-                    Toast.makeText(LoginActivity.this, "R.stirng.loginUnsuccessful", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, R.string.loginUnsuccessful, Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        leadToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
     }
