fbfedbfee110354c8bd0402021289fff94d43380
Trinh Dang Duong <duongtd0109@gmail.com>
2023-06-03 15:50:03 +0700

    Change background of lesson item & change layout of Splash screen

--- app/src/main/java/com/example/splus/CreateAssignmentActivity.java
+++ app/src/main/java/com/example/splus/CreateAssignmentActivity.java
@@ -10,5 +10,7 @@ public class CreateAssignmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_assignment);
+
+
     }
 }
