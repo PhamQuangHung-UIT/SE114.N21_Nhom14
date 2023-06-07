b3003e7129cddfacc872834009dbf77ff4fe6200
Phạm Quang Hưng <21522123@ms.uit.edu.vn>
2023-06-07 22:49:58 +0700

    Add CommentsFragment and RepliesFragment

--- app/src/main/java/com/example/splus/MainActivity.java
+++ app/src/main/java/com/example/splus/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.splus;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
@@ -87,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
+                super.onPageSelected(position);
                 navigation.getMenu().getItem(position).setChecked(true);
             }
         });
